a95b88c0ab975635335f2382dfa2f6bc527a1955
musai7 <musaishankar307@gmail.com>
2021-11-14 18:56:12 +0530

    Added

--- src/com/bridgeit/gambling/GamblingSimulator.java
+++ src/com/bridgeit/gambling/GamblingSimulator.java
@@ -8,18 +8,22 @@ public class GamblingSimulator {
 	private static final int IS_LOOSE = 0;
 
 	public static void main(String[] args) {
-	
-		 int winAmountINADay=0,looseAmountINADay=0;
+
+		int winAmountINADay = 0, looseAmountINADay = 0;
 		System.out.println("gambler starting with a stake of : $" + STAKE + " and bet in every game is : $" + BET);
-		double randomCheck =  Math.random()*2;
 		
-		switch((int)randomCheck) {
-		case IS_WIN:
-			winAmountINADay+=BET;
+		while ((winAmountINADay < (STAKE / 2)) && (looseAmountINADay > -(STAKE / 2))) {
+		
+			double randomCheck = Math.random() * 2;
+			switch ((int) randomCheck) {
+			case IS_WIN:
+				winAmountINADay += BET;
+				break;
+			default:
+				looseAmountINADay -= BET;
+			}
+			
 			System.out.println("win amount in a day is = " + winAmountINADay);
-			break;
-		default :
-			looseAmountINADay -= BET;
 			System.out.println("loose amount in a day = " + looseAmountINADay);
 		}
 	}
