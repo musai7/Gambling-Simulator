1e5325a324cbd5ce938f63424a54533f35980a69
musai7 <musaishankar307@gmail.com>
2021-11-14 13:13:04 +0530

    added : gambler stake and bet

---
+++ src/com/bridgeit/gambling/GamblingSimulator.java
@@ -0,0 +1,11 @@
+package com.bridgeit.gambling;
+
+public class GamblingSimulator {
+
+	private static final int STAKE=100;
+	private static final int BET=1;
+	
+	public static void main(String[] args) {
+		System.out.println("gambler starting with a stake of : $" + STAKE + " and bet in every game is : $" + BET);
+	}
+}
