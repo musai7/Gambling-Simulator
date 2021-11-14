bc0a112deb63404286740960bef7a6d3332256ea
musai7 <musaishankar307@gmail.com>
2021-11-14 20:15:09 +0530

    modified : 50% stake resign for the day

--- src/com/bridgeit/gambling/GamblingSimulator.java
+++ src/com/bridgeit/gambling/GamblingSimulator.java
@@ -9,22 +9,26 @@ public class GamblingSimulator {
 
 	public static void main(String[] args) {
 
-		int winAmountINADay = 0, looseAmountINADay = 0;
+		int totalStackValue = 100, dailyBet = 0;
 		System.out.println("gambler starting with a stake of : $" + STAKE + " and bet in every game is : $" + BET);
-		
-		while ((winAmountINADay < (STAKE / 2)) && (looseAmountINADay > -(STAKE / 2))) {
-		
+
+		while (dailyBet <= STAKE / 2) {
+
 			double randomCheck = Math.random() * 2;
 			switch ((int) randomCheck) {
-			case IS_WIN:
-				winAmountINADay += BET;
-				break;
-			default:
-				looseAmountINADay -= BET;
+				case IS_WIN:
+					totalStackValue += BET;
+					break;
+				default:
+					totalStackValue -= BET;
 			}
-			
-			System.out.println("win amount in a day is = " + winAmountINADay);
-			System.out.println("loose amount in a day = " + looseAmountINADay);
+
+			dailyBet += BET;
 		}
+		if (totalStackValue > STAKE)
+			System.out.println("win amount in a day is = " + (totalStackValue - STAKE));
+
+		else
+			System.out.println("loose amount in a day = " + (STAKE - totalStackValue));
 	}
 }
