77204dc70e0d0ec41dc1c1f9264c6a65e3efdd4a
musai7 <musaishankar307@gmail.com>
2021-11-14 13:36:55 +0530

    Added: win or loose logic implemented

--- src/com/bridgeit/gambling/GamblingSimulator.java
+++ src/com/bridgeit/gambling/GamblingSimulator.java
@@ -2,10 +2,25 @@ package com.bridgeit.gambling;
 
 public class GamblingSimulator {
 
-	private static final int STAKE=100;
-	private static final int BET=1;
-	
+	private static final int STAKE = 100;
+	private static final int BET = 1;
+	private static final int IS_WIN = 1;
+	private static final int IS_LOOSE = 0;
+
 	public static void main(String[] args) {
+	
+		 int winAmountINADay=0,looseAmountINADay=0;
 		System.out.println("gambler starting with a stake of : $" + STAKE + " and bet in every game is : $" + BET);
+		double randomCheck =  Math.random()*2;
+		
+		switch((int)randomCheck) {
+		case IS_WIN:
+			winAmountINADay+=BET;
+			System.out.println("win amount in a day is = " + winAmountINADay);
+			break;
+		default :
+			looseAmountINADay -= BET;
+			System.out.println("loose amount in a day = " + looseAmountINADay);
+		}
 	}
 }
